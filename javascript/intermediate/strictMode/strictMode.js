3c2c3582345efa44cfb4b18a21e6d3bb88dabc4a
Mathias Sommer <thiesen1999@gmail.com>
2025-11-23 12:12:58 +0100

    intermediate js

---
+++ javascript/intermediate/strictMode/strictMode.js
@@ -0,0 +1,11 @@
+// put this
+"use strict";
+// this enables strict mode'
+// can be used globally or in function etc
+
+// usecase
+
+let x = "thing"; // allowed
+
+y = "thing2"; // not allowed with strictmode
+
